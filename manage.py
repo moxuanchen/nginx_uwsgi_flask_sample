d4c8b8ea5bbdfc7062126107ecef8a5f465135ec
Aaron Chen <aaron.nehc@gmail.com>
2017-01-22 03:44:35 +0000

    Add uwsgi configure file.
    Signed-off-by: Aaron Chen <aaron.nehc@gmail.com>

--- manage.py
+++ manage.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import sys
+print sys.path
+
 from core.app import create_app
 from flask_script import Manager, Server
 
-manager = Manager(create_app())
+app = create_app()
+manager = Manager(app)
 
 
+"""
 if __name__ == "__main__":
     manager.add_command('server', Server)
     manager.run()
+"""
 
